0ca561890dddbfe41e7389d30b4c09214ef99709
Kaihua.Sheng <kaihua.sheng@magustek.com>
2019-02-14 11:26:07 +0800

    springboot-mybatisplus：集成mybatisplus

---
+++ springboot-mybatisplus/src/main/java/com/saber/SpringbootMybatisplusApplication.java
@@ -0,0 +1,16 @@
+package com.saber;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.saber.mapper")
+public class SpringbootMybatisplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisplusApplication.class, args);
+    }
+
+}
+
---
+++ springboot-mybatisplus/src/main/java/com/saber/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.saber.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.saber.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
